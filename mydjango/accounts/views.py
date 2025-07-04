604d25455e845735f717a27b0c7612788d60f322
youmi89 <imym1002@gmail.com>
2025-07-04 18:12:31 +0900

    Login 구현

--- mydjango/accounts/views.py
+++ mydjango/accounts/views.py
@@ -1,8 +1,13 @@
 
-from django.shortcuts import render
+# from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+# from django.shortcuts import render
 
 # Create your views here.
 def register(request):
     pass
-def login(request):
-    pass
+
+# def login(request):
+# pass
+
+login = LoginView.as_view()
