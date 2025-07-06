5b547ea2ce1216bdbceb2e7bcc93acc8e48be0b3
youmi89 <imym1002@gmail.com>
2025-07-06 17:13:07 +0900

    blog 앱의 view 함수 정의 및 URL 패턴 추가

--- mydjango/blog/urls.py
+++ mydjango/blog/urls.py
@@ -1,4 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("/write", views.post_new),
+
+    path("", views.post_list),
+
+    path("/<int:id>", views.post_detail),
+
+    path("/search/<str:tag>", views.post_search),
+
+    path("/edit/<int:id>", views.post_edit),
+
+    path("/delete/<int:id>", views.post_delete),
+]
--- mydjango/blog/views.py
+++ mydjango/blog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 게시글 작성 기능 구현
+# blog/write
+
+def post_new(request):
+    pass
+
+#  뷰 함수 이름은 현재 py 파일 내에서만 유일하면 된다.
+#  다른 이름과 겹치면, 다른 함수를 덮어쓴다. 주의!!
+
+# 게시글 목록 기능 구현
+# /blog
+def post_list(request):
+    pass
+
+# 게시글 상세보기 기능 구현
+#  /blog/<int:id>
+def post_detail(request, id: int):
+    pass
+
+# 게시글 검색 기능 구현
+#  /blog/search/<str:tag>
+def post_search(request, tag: str):
+    pass
+
+# 게시글 수정 기능 구현
+#  /blog/edit/<int:id>
+def post_edit(request, id: int):
+    pass
+
+# 게시글 삭제 기능 구현
+#  /blog/delete/<int:id>
+def post_delete(request, id: int):
+    pass
